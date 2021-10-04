ac7de2713f856bfee9ebeed4854895e2703e9825
Huy Trương Thành <ripker1805@gmail.com>
2021-10-04 13:51:14 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as dotenv from "dotenv";
 import * as swaggerUi from "swagger-ui-express";
 import * as swaggerJsDoc from "swagger-jsdoc";
+import * as cors from "cors";
 import Router from "./routes";
 dotenv.config();
 
@@ -26,6 +27,7 @@ const options = {
 const specs = swaggerJsDoc(options);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api", Router);
 const port = process.env.PORT || "4000";
